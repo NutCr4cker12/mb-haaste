d5a9ffa41b36a2e52471381d6434f7fa009c79d6
Kimi Heinonen <kimi.heinonen@gmail.com>
2023-11-25 17:37:24 +0200

    Implemented Customers table filtering

--- frontend/src/CustomerTable.jsx
+++ frontend/src/CustomerTable.jsx
@@ -1,12 +1,38 @@
 import PropTypes from 'prop-types'
 import { useNavigate } from 'react-router-dom';
 import MBTodo from './MBTodo';
+import { useState } from 'react';
 
 const Table = ({ customers }) => {
-  const navigate = useNavigate();
-
+  const navigate = useNavigate()
+  const [filters, setFilters] = useState(() => {
+    const storageFilters = window.localStorage.getItem('customerTableFilters')
+    if (!storageFilters) {
+      return { isActive: 'None' }
+    }
+    return JSON.parse(storageFilters)
+  })
+  
   const clicker = (customer) => {
     navigate(customer.id);
+  }
+
+  const setIsActiveFilter = (event) => {
+    let value = event.target.value
+    // Don't set true | false as string values
+    if (value !== 'None') {
+      value = value === 'true'
+    }
+    
+    setFilters(prevState => {
+      const newFilters = {
+        ...prevState,
+        isActive: value
+      }
+      window.localStorage.setItem('customerTableFilters', JSON.stringify(newFilters))
+      return newFilters
+    })
+    
   }
 
   return (
@@ -16,8 +42,14 @@ const Table = ({ customers }) => {
           <i className="bi bi-filter" />
           {' '}
           Filters
-          <div>
-            <MBTodo isCompleted={false} task='Create solution to filters customers by activity' />
+          <div className='d-flex mt-3 align-items-center'>
+            <MBTodo isCompleted={true} task='Create solution to filters customers by activity' />
+            <p className='fw-bold col-1 mb-0'>Is Active</p>
+            <select className="form-select" value={filters.isActive} onChange={setIsActiveFilter}>
+              <option value='None'>Any</option>
+              <option value={false}>❌</option>
+              <option value={true}>✅</option>
+            </select>
           </div>
         </div>
       </div>
@@ -31,20 +63,22 @@ const Table = ({ customers }) => {
           </tr>
         </thead>
         <tbody>
-          {customers.map((customer, index) => {
-            return (
-              <tr
-                key={index}
-                className=''
-                onClick={() => clicker(customer)}
-              >
-                <td scope="row">{index + 1}</td>
-                <td>{customer.name}</td>
-                <td>{customer.country}</td>
-                <td>{customer.isActive ? '✅' : '❌'}</td>
-              </tr>
-            )
-          })}
+          {customers
+            .filter(customer => filters.isActive === 'None' || customer.isActive === filters.isActive)
+            .map((customer, index) => {
+              return (
+                <tr
+                  key={index}
+                  className=''
+                  onClick={() => clicker(customer)}
+                >
+                  <td scope="row">{index + 1}</td>
+                  <td>{customer.name}</td>
+                  <td>{customer.country}</td>
+                  <td>{customer.isActive ? '✅' : '❌'}</td>
+                </tr>
+              )
+            })}
         </tbody>
       </table>
     </>
